a73c9ad58cfcc428f826fe4ac7648d85589fe9d7
StayHungryStayFoolish <bonismo@hotmail.com>
2017-11-01 10:29:04 +0800

    commit

---
+++ ReadAndGeneratorExcel/src/main/java/com/excel/bean/ExportExcelBean.java
@@ -0,0 +1,10 @@
+package com.excel.bean;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 上午10:25 on 17/11/1.
+ *
+ * 如果生成 Excel 使用该类，根据 Excel 列建立对应的域
+ */
+public class ExportExcelBean {
+}
---
+++ ReadAndGeneratorExcel/src/main/java/com/excel/bean/ImportExcelBean.java
@@ -0,0 +1,11 @@
+package com.excel.bean;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 上午10:24 on 17/11/1.
+ *
+ * 如果读取 Excel 使用该类，根据 Excel 列建立对应的域
+ */
+public class ImportExcelBean {
+
+}
