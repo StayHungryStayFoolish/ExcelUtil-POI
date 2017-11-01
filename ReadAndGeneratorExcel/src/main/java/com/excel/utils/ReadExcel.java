a8d6ad6f78100ba7f25222162e3d9d354e3b76b5
StayHungryStayFoolish <bonismo@hotmail.com>
2017-11-01 11:37:26 +0800

    commit

--- ReadAndGeneratorExcel/src/main/java/com/excel/utils/ReadExcel.java
+++ ReadAndGeneratorExcel/src/main/java/com/excel/utils/ReadExcel.java
@@ -8,7 +8,6 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.text.DecimalFormat;
@@ -20,39 +19,55 @@ import java.util.Map;
 /**
  * Created by bonismo@hotmail.com
  * 上午10:32 on 17/11/1.
- *
- * 分析并读取 Excel
+ * <p>
+ * 分析并读取 Excel，只能读取 xls 格式
  */
 public class ReadExcel {
 
     private static final String EXCEL_PATH = "/Users/bonismo/Desktop/1023.xls";
 
-    public static void read(String filePath) {
+    public static void main(String[] args) {
+        try {
+            parseToRead(EXCEL_PATH);
+        } catch (InvocationTargetException | NoSuchMethodException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 根据传入文件路径，分析并读取文件
+     *
+     * @param filePath
+     * @throws InvocationTargetException
+     * @throws NoSuchMethodException
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     */
+    public static void parseToRead(String filePath) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         File path = new File(filePath);
 
         /*********** 注：****************/
         // 根据 Excel 列数量，值需要更改
         // 获得 Excel
-        String[] columnName = new String[]{Constant.EXPORT_ORG_CODE,Constant.EXPORT_ORG_NAME, Constant.EXPORT_USER_CODE, Constant.EXPORT_USER_NAME};
+        String[] columnName = new String[]{Constant.EXPORT_ORG_CODE, Constant.EXPORT_ORG_NAME, Constant.EXPORT_USER_CODE, Constant.EXPORT_USER_NAME};
 
         List<ImportExcelBean> list = null;
-        try {
-            list = ReadExcel.parse(ImportExcelBean.class, columnName, path);
-            for (ImportExcelBean excelBean : list) {
-                excelBean.getUserName();
-                System.out.println(excelBean.getUserName());
-            }
-        } catch (IllegalAccessException | InvocationTargetException | InstantiationException | NoSuchMethodException e) {
-            e.printStackTrace();
+        list = ReadExcel.parse(ImportExcelBean.class, columnName, path);
+        for (ImportExcelBean excelBean : list) {
+            // 可以输出其他属性
+            String userName = excelBean.getUserName();
+            System.out.println(userName);
         }
     }
 
     /**
      * 根据传入的 Bean 类，域字段，文件路径分析 Excel
-     * @param cc Excel 对应的类模型（利用反应，反射机制处理）
+     *
+     * @param cc         Excel 对应的类模型（利用反应，反射机制处理）
      * @param columnName Excel 列 对应的 Bean 域
-     * @param file Excel 文件路径
-     * @param <T> 泛型
+     * @param file       Excel 文件路径
+     * @param <T>        泛型
      * @return 返回 Bean 类的 List 集合
      */
     public static <T> List<T> parse(Class<T> cc, String[] columnName, File file) throws IllegalAccessException, InvocationTargetException, InstantiationException, SecurityException, NoSuchMethodException {
@@ -72,6 +87,7 @@ public class ReadExcel {
 
     /**
      * 根据传入的文件路径，分析 Excel
+     *
      * @param file 文件路径
      * @return String 数组的 List 集合
      */
